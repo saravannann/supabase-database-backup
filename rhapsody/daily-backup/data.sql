3b438a81ae9bb5e814683e348352fe44b847e68c
saravannann <saravannann@users.noreply.github.com>
2026-09-08 03:18:18 +0000

    Supabase backup

--- rhapsody/daily-backup/data.sql
+++ rhapsody/daily-backup/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict sXQLPoipXblCidlhMJiVDaQmcL5UTa41PtiUi2swtZeOxjTubq81KRLRvqiMNgU
+-- \restrict kVwNX4LzN6GfPJYzPOseY8syFVLWX5zMzscEomkmxxbGWHNGOXEpxpcaeErPKd4
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -1446,6 +1446,6 @@ SELECT pg_catalog.setval('"public"."vip_ticket_sequence"', 44, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict sXQLPoipXblCidlhMJiVDaQmcL5UTa41PtiUi2swtZeOxjTubq81KRLRvqiMNgU
+-- \unrestrict kVwNX4LzN6GfPJYzPOseY8syFVLWX5zMzscEomkmxxbGWHNGOXEpxpcaeErPKd4
 
 RESET ALL;
